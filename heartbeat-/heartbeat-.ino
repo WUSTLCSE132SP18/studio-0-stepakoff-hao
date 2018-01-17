1d6f6d8004d04c6bb1f9a5d180b0e097f47440e6
jestepakoff <jestepakoff@URB218-13.seasad.wustl.edu>
2018-01-17 17:25:13 -0600

    End of Studio 0

---
+++ heartbeat-/heartbeat-.ino
@@ -0,0 +1,14 @@
+int sec = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(sec);
+  Serial.println(" sec have elapsed");
+  sec++;
+  delay(1000);
+}
